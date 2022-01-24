ad2d919bb785404d215c13c4163388fa72d9e9eb
Eoin Carrick <22carri99@gmail.com>
2022-01-24 03:22:06 -0800

    master

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,27 +1,51 @@
 import "../components/style/Sidebar.css";
 import React from "react";
 import { Link } from "react-router-dom";
-import { mdiViewDashboard } from "react-icons/md";
+import {
+  FaFolder,
+  FaSearch,
+  FaPhoneAlt,
+  FaHistory,
+  FaStar,
+  FaUserAlt,
+  FaSignOutAlt,
+} from "react-icons/fa";
 
 function Sidebar() {
   return (
     <div className="sideBar">
       <div className="logoContainer">
-        <span></span>
-        <div>Logo</div>
+        <div className="logo"></div>
+        <div className="logo"></div>
+        <div className="logo"></div>
       </div>
 
       <div className="sideList">
-        <Link to="/Dashboard">Dashboard</Link>
-        <Link to="/Search_Users">Search Users</Link>
-        <Link to="/Call_Monitor">Call Monitor</Link>
-        <Link to="/History">History</Link>
-        <Link to="/Leads">Leads</Link>
-        <Link to="/Profile">Profile</Link>
+        <Link className="link" to="/Dashboard">
+          <FaFolder className="icons" /> Dashboard
+        </Link>
+        <Link className="link" to="/Search_Users">
+          <FaSearch className="icons" /> Search Users
+        </Link>
+        <Link className="link" to="/Call_Monitor">
+          <FaPhoneAlt className="icons" /> Call Monitor
+        </Link>
+        <Link className="link" to="/History">
+          <FaHistory className="icons" /> History
+        </Link>
+        <Link className="link" to="/Leads">
+          <FaStar className="icons" /> Leads
+        </Link>
+        <Link className="link" to="/Profile">
+          <FaUserAlt className="icons" /> Profile
+        </Link>
       </div>
 
       <div className="logContainer">
-        <mdiViewDashboard /> <Link to="/Logout">Logout</Link>
+        <mdiViewDashboard />
+        <Link className="link" to="/Logout">
+          <FaSignOutAlt className="icons" /> Logout
+        </Link>
       </div>
     </div>
   );
