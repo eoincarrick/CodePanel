f28b1b7ece7e513405a1df68e46a9c711dd5584e
Eoin Carrick <22carri99@gmail.com>
2022-01-26 13:38:55 -0800

    master

--- src/House/Contact.js
+++ src/House/Contact.js
@@ -9,7 +9,52 @@ function Contact() {
         <Navbar />
       </div>
 
-      <div className="contactContainer"></div>
+      <div className="contactContainer">
+        <form>
+          <div className="firstAndLastName">
+            <div className="inputAndFname">
+              <label htmlFor="fname">First Name</label>
+              <input
+                className="inputs"
+                type="text"
+                id="fname"
+                placeholder="eg. Eoin"
+                required
+              />
+            </div>
+
+            <div className="inputLname">
+              <label htmlFor="lname">Last Name</label>
+              <input
+                className="inputs"
+                type="text"
+                id="lname"
+                placeholder="eg. Carrick"
+                required
+              />
+            </div>
+          </div>
+
+          <label htmlFor="email">E-Mail Address</label>
+          <input
+            className="inputs"
+            type="email"
+            id="email"
+            placeholder="eg. examples@mail.com.."
+            required
+          />
+
+          <label htmlFor="reason">Reason For Contacting Us.</label>
+          <textarea
+            cols="30"
+            rows="10"
+            placeholder="Let us know how we can help😊"
+            required
+          ></textarea>
+
+          <input className="submit" type="submit" value="Submit"></input>
+        </form>
+      </div>
     </div>
   );
 }
