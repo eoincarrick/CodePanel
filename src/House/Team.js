262e5ab588696ce0c316019233f27628822a4019
Eoin Carrick <22carri99@gmail.com>
2022-01-26 11:13:25 -0800

    commit

--- src/House/Team.js
+++ src/House/Team.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Navbar from "../components/Navbar";
 import "../House/style/Team.css";
+import eoincarrick from "../assets/e.jpg";
+import seniorbisamrk from "../assets/d.jpg";
+import unknown from "../assets/a.jpg";
+import { FaFacebook, FaTwitter, FaInstagram } from "react-icons/fa";
 
 function Team() {
   return (
@@ -12,7 +16,35 @@ function Team() {
       <div className="teamContainer">
         <div className="profileOne">
           <div className="profileImg">
-            <img src="" alt="" />
+            <img className="teamProfile" src={seniorbisamrk} alt="" />
+          </div>
+
+          <div className="header4">
+            <h4> Bismark Amanor</h4>
+          </div>
+
+          <div className="profileDescription">
+            <p>
+              Software Developer | Mobile App Developer | Web Developer | Skills
+              - React Native, React, HTML, CSS, PHP, Firebase
+            </p>
+          </div>
+
+          <div className="socialHandle">
+            <div className="instagram">
+              <FaInstagram />
+            </div>
+            <div className="twitter">
+              <FaTwitter />
+            </div>
+            <div className="facebook">
+              <FaFacebook />
+            </div>
+          </div>
+        </div>
+        <div className="profileOne">
+          <div className="profileImg">
+            <img className="teamProfile" src={eoincarrick} alt="" />
           </div>
 
           <div className="header4">
@@ -25,6 +57,47 @@ function Team() {
               Tailwind, Bootstrap. I really love what I do especially
               debugging😂. Knowledge: D3.js
             </p>
+          </div>
+
+          <div className="socialHandle">
+            <div className="instagram">
+              <FaInstagram />
+            </div>
+            <div className="twitter">
+              <FaTwitter />
+            </div>
+            <div className="facebook">
+              <FaFacebook />
+            </div>
+          </div>
+        </div>
+        <div className="profileOne">
+          <div className="profileImg">
+            <img className="teamProfile" src={unknown} alt="" />
+          </div>
+
+          <div className="header4">
+            <h4>Unknown</h4>
+          </div>
+
+          <div className="profileDescription">
+            <p>
+              Someone else koraa😂. Who want their name here. Dm me quick. Ill
+              be sharing on the internet, with your social handles connected, so
+              you can get follow for free.
+            </p>
+          </div>
+
+          <div className="socialHandle">
+            <div className="instagram">
+              <FaInstagram />
+            </div>
+            <div className="twitter">
+              <FaTwitter />
+            </div>
+            <div className="facebook">
+              <FaFacebook />
+            </div>
           </div>
         </div>
       </div>
