c72ed196338deeb4d167bebc48787f5f67e3d854
Eoin Carrick <22carri99@gmail.com>
2022-02-05 08:44:38 +0000

    completed

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,11 +1,62 @@
 import "../pages/style/Dashboard.css";
-import React from "react";
+import React, { useState } from "react";
 import Sidebar from "../components/Sidebar.js";
 import { Link } from "react-router-dom";
 import Search from "../components/Search";
 import Profile from "../components/Profile";
+import ReactApexChart from "react-apexcharts";
 
 function Dashboard() {
+  const [state, setState] = useState({
+    series: [
+      {
+        name: "GitHub",
+        data: [31, 40, 28, 51, 42, 109, 100],
+      },
+      {
+        name: "GitLab",
+        data: [11, 32, 45, 32, 34, 52, 41],
+      },
+      {
+        name: "BitBucket",
+        data: [43, 65, 45, 12, 4, 2, 1],
+      },
+      {
+        name: "CodePen",
+        data: [16, 2, 49, 32, 34, 100, 99],
+      },
+    ],
+    options: {
+      chart: {
+        height: 350,
+        type: "area",
+      },
+      dataLabels: {
+        enabled: false,
+      },
+      stroke: {
+        curve: "smooth",
+      },
+      xaxis: {
+        type: "datetime",
+        categories: [
+          "2018-09-19T00:00:00.000Z",
+          "2018-09-19T01:30:00.000Z",
+          "2018-09-19T02:30:00.000Z",
+          "2018-09-19T03:30:00.000Z",
+          "2018-09-19T04:30:00.000Z",
+          "2018-09-19T05:30:00.000Z",
+          "2018-09-19T06:30:00.000Z",
+        ],
+      },
+      tooltip: {
+        x: {
+          format: "dd/MM/yy HH:mm",
+        },
+      },
+    },
+  });
+
   return (
     <div className="dashBoard">
       <Sidebar />
@@ -41,8 +92,14 @@ function Dashboard() {
         <div className="workSpace">
           <div className="wholeChart">
             <div className="setLocation">
-              <div className="chart"></div>
-
+              <div id="chart">
+                <ReactApexChart
+                  options={state.options}
+                  series={state.series}
+                  type="area"
+                  height={350}
+                />
+              </div>
               <div className="chartExplained"></div>
             </div>
 
