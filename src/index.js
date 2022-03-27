be709a02cb15b239e3a3b55ef714f29c2ac24840
Eoin Carrick <22carri99@gmail.com>
2022-03-27 01:51:55 +0000

    code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 const root = document.querySelector('#codePanel');
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <Router>
+    <Routes>
+      <Route path='*' element={<App />} />
+    </Routes>
+  </Router>,
+
+  root
+);
